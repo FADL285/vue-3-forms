ff091165f4902e8e4c71e9eba94f34041123113a
Mohamed Fadl <mohamed.fadl2852@gmail.com>
2022-05-03 09:10:55 +0200

    Added VeeValidate for form validation

---
+++ src/views/LoginForm.vue
@@ -0,0 +1,23 @@
+<template>
+  <form @submit.prevent="onSubmit">
+    <BaseInput label="Email" type="email" v-model="email" :error="emailError" />
+
+    <BaseInput label="Password" type="password" />
+
+    <BaseButton type="submit" class="-fill-gradient"> Submit </BaseButton>
+  </form>
+</template>
+
+<script>
+export default {
+  setup() {
+    function onSubmit() {
+      alert('Submitted');
+    }
+
+    return {
+      onSubmit
+    };
+  }
+};
+</script>
