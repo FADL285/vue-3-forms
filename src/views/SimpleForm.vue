d9ddd870920788dd4c61c554f2494cb48f85a3f4
Mohamed Fadl <mohamed.fadl2852@gmail.com>
2022-05-03 07:50:30 +0200

    Create BaseCheckbox component

--- src/views/SimpleForm.vue
+++ src/views/SimpleForm.vue
@@ -31,13 +31,11 @@
 
       <h3>Extras</h3>
       <div>
-        <input type="checkbox" v-model="event.extras.catering" class="field" />
-        <label>Catering</label>
+        <BaseCheckbox v-model="event.extras.catering" label="Catering" />
       </div>
 
       <div>
-        <input type="checkbox" v-model="event.extras.music" class="field" />
-        <label>Live music</label>
+        <BaseCheckbox v-model="event.extras.music" label="Live music" />
       </div>
 
       <button class="button -fill-gradient" type="submit">Submit</button>
