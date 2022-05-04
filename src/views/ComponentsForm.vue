f18dcd636faae6aaa01f1e39c69284dadb9efa71
Mohamed Fadl <mohamed.fadl2852@gmail.com>
2022-05-04 21:47:53 +0200

    Added yup lib and use it to validate

--- src/views/ComponentsForm.vue
+++ src/views/ComponentsForm.vue
@@ -1,26 +1,41 @@
 <template>
   <div>
     <h1>Create an Event</h1>
-    <form>
+    <form @submit="submit">
       <BaseSelect
         label="Select a category"
         :options="categories"
-        v-model=""
-        :error=""
+        v-model="category"
+        :error="errors.category"
       />
 
       <h3>Name & describe your event</h3>
-      <BaseInput label="Title" v-model="" :error="" type="text" />
+      <BaseInput
+        label="Title"
+        v-model="title"
+        :error="errors.title"
+        type="text"
+      />
 
-      <BaseInput label="Description" v-model="" :error="" type="text" />
+      <BaseInput
+        label="Description"
+        v-model="description"
+        :error="errors.description"
+        type="text"
+      />
 
       <h3>Where is your event?</h3>
-      <BaseInput label="Location" v-model="" :error="" type="text" />
+      <BaseInput
+        label="Location"
+        v-model="location"
+        :error="errors.location"
+        type="text"
+      />
 
       <h3>Are pets allowed?</h3>
       <BaseRadioGroup
-        v-model=""
-        :error=""
+        v-model="pets"
+        :error="errors.pets"
         name="pets"
         :options="[
           { value: 1, label: 'Yes' },
@@ -30,11 +45,19 @@
 
       <h3>Extras</h3>
       <div>
-        <BaseCheckbox label="Catering" v-model="" :error="" />
+        <BaseCheckbox
+          label="Catering"
+          v-model="catering"
+          :error="errors.catering"
+        />
       </div>
 
       <div>
-        <BaseCheckbox label="Live music" v-model="" :error="" />
+        <BaseCheckbox
+          label="Live music"
+          v-model="music"
+          :error="errors.music"
+        />
       </div>
 
       <div>
@@ -47,28 +70,66 @@
 </template>
 
 <script>
+import { useField, useForm } from 'vee-validate';
+import { object, string, number, boolean } from 'yup';
+
 export default {
+  data() {
+    return {
+      categories: [
+        'sustainability',
+        'nature',
+        'animal welfare',
+        'housing',
+        'education',
+        'food',
+        'community'
+      ]
+    };
+  },
   setup() {
-    const required = (value) => {
-      const requiredMessage = 'This field is required';
-      if (value === undefined || value === null) return requiredMessage;
-      if (!String(value).length) return requiredMessage;
+    const validationSchema = object({
+      category: string().required(),
+      title: string().required('A cool title is required.').min(3),
+      description: string().required(),
+      location: string(),
+      pets: number(),
+      catering: boolean(),
+      music: boolean()
+    });
 
-      return true;
+    const { handleSubmit, errors } = useForm({
+      validationSchema,
+      initialValues: {
+        pets: 1,
+        catering: false,
+        music: false
+      }
+    });
+
+    const { value: category } = useField('category');
+    const { value: title } = useField('title');
+    const { value: description } = useField('description');
+    const { value: location } = useField('location');
+    const { value: pets } = useField('pets');
+    const { value: catering } = useField('catering');
+    const { value: music } = useField('music');
+
+    const submit = handleSubmit((values) => {
+      console.log('Form Data that wanna to handle', values);
+    });
+
+    return {
+      category,
+      title,
+      description,
+      location,
+      pets,
+      catering,
+      music,
+      errors,
+      submit
     };
-
-    const minLength = (number, value) => {
-      if (String(value).length < number)
-        return 'Please type at least ' + number + ' characters';
-
-      return true;
-    };
-
-    const anything = () => {
-      return true;
-    };
-
-    return {};
   }
 };
 </script>
