2a40694bafd8312d6b3b4815a028718936fada2b
Mohamed Fadl <mohamed.fadl2852@gmail.com>
2022-05-04 20:11:53 +0200

    Added base validation for login form

--- src/views/LoginForm.vue
+++ src/views/LoginForm.vue
@@ -1,21 +1,63 @@
 <template>
-  <form @submit.prevent="onSubmit">
-    <BaseInput label="Email" type="email" v-model="email" :error="emailError" />
+  <h1>Login</h1>
 
-    <BaseInput label="Password" type="password" />
+  <form @submit.prevent="onSubmit">
+    <BaseInput
+      label="Email"
+      type="email"
+      v-model="email"
+      :error="emailErrorMessage"
+    />
+
+    <BaseInput
+      label="Password"
+      type="password"
+      v-model="password"
+      :error="passwordErrorMessage"
+    />
 
     <BaseButton type="submit" class="-fill-gradient"> Submit </BaseButton>
   </form>
 </template>
 
 <script>
+import { useForm, useField } from 'vee-validate';
+
 export default {
   setup() {
     function onSubmit() {
       alert('Submitted');
     }
 
+    // Validation Schema
+    const validationSchema = {
+      email: (value) => {
+        if (value && value.trim()) {
+          return true;
+        }
+        return 'Email is required';
+      },
+      password: (value) => {
+        if (value && value.trim()) {
+          return true;
+        }
+        return 'Password is required';
+      }
+    };
+
+    useForm({
+      validationSchema
+    });
+
+    const { errorMessage: emailErrorMessage, value: email } = useField('email');
+    const { errorMessage: passwordErrorMessage, value: password } =
+      useField('password');
+
     return {
+      email,
+      emailErrorMessage,
+      password,
+      passwordErrorMessage,
       onSubmit
     };
   }
